df059ab0f5a37138969564d783ee644bab1bc475
Banty2002 <bkbanty2002@gmail.com>
2024-05-05 10:12:27 +0530

    5052024

--- Hello.java
+++ Hello.java
@@ -2,7 +2,9 @@ class Hello{
 
     public static void main(String [] args){ 
 
-        System.out.println("Hello"); 
+        for(int i=1; i<=10; i++){
+            System.out.println("Hello"); 
+        }
 
     } 
 
